7ae9fa0dddf2eb340e84d41691bf94d7a9b786fd
Petr Usoltsev <usoltsev05@mail.ru>
2023-05-07 09:43:58 +0800

    add date,literal,mmap

--- example.c
+++ example.c
@@ -1,77 +1,96 @@
 // re2c $INPUT -o $OUTPUT -8 --case-ranges -i
 #include <assert.h>
-#include <stdint.h>
+//#include <stdint.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 enum TOKEN {
 ERROR //0
 , NUMBER //1
 , ID //2
-, TEST //3
+, DATE //3
+, LITERAL //4
+, TEST //5
  };
+
+void
+print_error (const char *format, ...)
+{
+  va_list argptr;
+  va_start (argptr, format);
+  vprintf (format, argptr);
+  va_end (argptr);
+  exit (EXIT_FAILURE);
+}
 
 /*!include:re2c "unicode_categories.re" */
 
 enum TOKEN
-lex (const char *s, const char **new_pos)
+lex (const char **start_pos, const char **end_pos)
 {
-  const char *YYCURSOR = s, *YYMARKER;
-  *new_pos = s;
+  const char *YYCURSOR = *start_pos, *YYMARKER;
+  *end_pos = *start_pos;
   /*!re2c
      re2c:define:YYCTYPE = 'unsigned char';
      re2c:yyfill:enable = 0;
 
-     // Simplified "Unicode Identifier and Pattern Syntax"
-     // (see https://unicode.org/reports/tr31)
-     //id_start_utf    = L | Nl | [$_];
-     //id_continue_utf = id_start_utf | Mn | Mc | Nd | Pc | [\u200D\u05F3];
-     //identifier_utf  = id_start_utf id_continue*;
-
-     one = "!";
-     two = ";";
-     ws = " ";
-     splitters = one two ws;
-     test = [splitters]+;
      digit = [0-9];
      digits = digit+;
      number = digits([\.]digits)?;
-     id = [^ !;\.]+;
-     number { *new_pos = YYCURSOR; return NUMBER; }
-     id { *new_pos = YYCURSOR; return ID; }
-     *          { return ERROR; }
+     id = [^ ;\.'"]+;
+     date = ['] [^']+ ['];
+     literal = ["] ([^"] | [""])* ["];
+
+     number { *end_pos = YYCURSOR; return NUMBER; }
+     date { *end_pos = YYCURSOR; return DATE; }
+     literal { *end_pos = YYCURSOR; return LITERAL; }
+     id { *end_pos = YYCURSOR; return ID; }
+     *  { return ERROR; }
    */
 }
 
 int
-main ()
+main (int argc, char** argv)
 {
-  const char *new_pos = NULL;
-  enum TOKEN res = ERROR;
 
-#if 1
-  const char *lexeme = "0123.222A ";
-#endif
-#if 0
-  const char *lexeme = "IF 1";
-#endif
-#if 0
-  const char *lexeme = "ab1c ";
-#endif
-#if 0
-  const char *lexeme = "абв ";
-#endif
-#if 0
-  const char *lexeme = "1_ЫдентифиZкатор 1";
-#endif
+  if (argc != 2)
+    print_error ("%s", "Не указан входной файл");
 
-#if 0
-  const char *lexeme = ";!;";
-#endif
+  struct stat statbuf;
+  int fd;
+  char buf[4096] = { 0 };
 
+  if ((fd = open (argv[1], O_RDONLY)) < 0)
+    print_error ("невозможно открыть %s для чтения",
+		 argv[1]);
 
+  if (fstat (fd, &statbuf) < 0)
+    print_error
+      ("Ошибка вызова функции fstat:%s у файла %s\n",
+       strerror (errno), argv[1]);
 
-  res = lex (lexeme, &new_pos);
-  printf ("%s:%d new_pos:%s\n", lexeme, res, new_pos);
+  void *src;
+  if ((src =
+       mmap (0, statbuf.st_size, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED)
+    print_error ("%s\n",
+		 "Ошибка вызова функции mmap для входного файла");
 
-  return 0;
+  const char *start_pos = (const char *) src;
+  const char *end_pos = NULL;
+
+  enum TOKEN res = lex (&start_pos, &end_pos);
+  size_t n = end_pos - start_pos;
+  memcpy (buf, start_pos, n);
+  buf[n] = '\0';
+
+  printf ("%s\nres:%d\n", buf, res);
+
+  munmap (src, statbuf.st_size);
+  return EXIT_SUCCESS;
 }
